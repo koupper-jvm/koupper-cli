ffe6c42c0a2266d1a2350bfad3138c8141f72825
Jacob Guzmán Acosta <jacob.gacosta@gmail.com>
2019-12-30 22:00:51 -0600

    Apply print formatting

--- src/main/kotlin/io/kup/installer/UndefinedCommand.kt
+++ src/main/kotlin/io/kup/installer/UndefinedCommand.kt
@@ -10,6 +10,6 @@ class UndefinedCommand : Command() {
     }
 
     override fun execute(vararg args: String) {
-        print("$RED_BACKGROUND_203$ANSI_WHITE The command '${args[0]}' is undefined. $ANSI_RESET")
+        println("\n$RED_BACKGROUND_203$ANSI_WHITE The command '${args[0]}' is undefined. $ANSI_RESET \n")
     }
 }
