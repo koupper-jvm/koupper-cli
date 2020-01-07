ff091fefd7a3b6b547458fc473ab5989ddd46405
Jacob Guzmán Acosta <jacob.gacosta@gmail.com>
2020-01-06 23:20:30 -0600

    Add creation type for kotlin option

--- src/main/kotlin/io/kup/installer/languages/KotlinOption.kt
+++ src/main/kotlin/io/kup/installer/languages/KotlinOption.kt
@@ -1,9 +1,13 @@
 package io.kup.installer.languages
 
+import io.kup.installer.ANSIColors.ANSI_BLACK
 import io.kup.installer.ANSIColors.ANSI_RESET
 import io.kup.installer.ANSIColors.ANSI_WHITE
 import io.kup.installer.ANSIColors.ANSI_YELLOW_229
+import io.kup.installer.ANSIColors.YELLOW_BACKGROUND_222
 import io.kup.installer.Wizard
+import io.kup.installer.constructions.ProjectOption
+import io.kup.installer.constructions.ScriptOption
 
 class KotlinOption : Wizard {
     override fun init() {
@@ -11,9 +15,11 @@ class KotlinOption : Wizard {
 
         print(
             """
-                
+
+
+                Create a
                 $ANSI_YELLOW_229
-                1.- Project
+                1.- Project (default)
                 2.- Script $ANSI_RESET
                 
                 Choose an option: 
@@ -21,5 +27,20 @@ class KotlinOption : Wizard {
         )
 
         val option = readLine()
+
+        when {
+            option!!.isEmpty() -> {
+                print("$YELLOW_BACKGROUND_222$ANSI_BLACK Using default creation. $ANSI_RESET")
+
+                ProjectOption().init()
+            }
+            option == "1" -> ProjectOption().init()
+            option == "2" -> ScriptOption().init()
+            else -> {
+                println("\n$YELLOW_BACKGROUND_222$ANSI_BLACK Option $option is not valid. Using default creation.$ANSI_RESET\n")
+
+                ProjectOption().init()
+            }
+        }
     }
 }
