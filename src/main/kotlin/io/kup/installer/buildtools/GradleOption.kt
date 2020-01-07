e9e0a54d9177ea0015b6c952a0ea5ad28dd3d2ac
Jacob Guzmán Acosta <jacob.gacosta@gmail.com>
2020-01-06 23:21:32 -0600

    Add 'build tools' package and classes

---
+++ src/main/kotlin/io/kup/installer/buildtools/GradleOption.kt
@@ -0,0 +1,9 @@
+package io.kup.installer.buildtools
+
+import io.kup.installer.Wizard
+
+class GradleOption : Wizard {
+    override fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
