d6dfb72da28a0e4bd8b9d71eb3b76a045cb3fa70
Jacob Guzmán Acosta <jacob.gacosta@gmail.com>
2020-01-07 22:56:33 -0600

    Require the file name for script creation

--- src/main/kotlin/io/kup/installer/constructions/ScriptOption.kt
+++ src/main/kotlin/io/kup/installer/constructions/ScriptOption.kt
@@ -6,8 +6,30 @@ import io.kup.installer.Wizard
 
 class ScriptOption : Wizard {
     override fun init() {
-        print("${ANSI_YELLOW_229}\nName of file: ${ANSIColors.ANSI_RESET}")
+        var fileName: String?
 
-        val fileName = readLine()
+        do {
+            print("${ANSI_YELLOW_229}\nName of file: ${ANSIColors.ANSI_RESET}")
+
+            fileName = readLine()
+
+            if (fileName!!.isEmpty()) {
+                print("\n${ANSIColors.YELLOW_BACKGROUND_222}${ANSIColors.ANSI_BLACK} The name of file can not be empty. ${ANSIColors.ANSI_RESET}\n")
+            }
+        } while (fileName!!.isEmpty())
+
+        val finalFileName = this.sanitizeFileName(fileName)
+
+        print("\n${ANSIColors.ANSI_WHITE}file created with path: " + System.getProperty("user.dir") + "/$finalFileName${ANSIColors.ANSI_RESET}\n")
+    }
+
+    private fun sanitizeFileName(fileName: String): String {
+        val hasExtension = fileName.contains(".kt")
+
+        if (hasExtension) {
+            return fileName
+        }
+
+        return fileName.plus(".kt")
     }
 }
