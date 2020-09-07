61f1c87ad8b4387b3b66dd3934fc600dae3af80b
Jacob G. Acosta <jacob.gacosta@gmail.com>
2020-09-07 17:45:01 -0500

    Accept relative paths

--- src/main/kotlin/com/koupper/installer/commands/RunCommand.kt
+++ src/main/kotlin/com/koupper/installer/commands/RunCommand.kt
@@ -1,15 +1,18 @@
 package com.koupper.installer.commands
 
 import com.koupper.installer.ANSIColors
-import com.koupper.installer.ANSIColors.ANSI_BLACK
 import com.koupper.installer.ANSIColors.ANSI_RED
 import com.koupper.installer.ANSIColors.ANSI_RESET
-import com.koupper.installer.ANSIColors.YELLOW_BACKGROUND_222
+import com.koupper.installer.ANSIColors.ANSI_YELLOW_229
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.stream.Collectors
 import kotlin.system.exitProcess
+
+val isSingleFileName: (String) -> Boolean = {
+    it.contains("^[a-zA-Z0-9]+.kts$".toRegex())
+}
 
 class RunCommand : Command() {
     init {
@@ -21,39 +24,71 @@ class RunCommand : Command() {
     }
 
     override fun execute(vararg args: String) {
+        if (args.isNotEmpty() && args[0].isNotEmpty()) {
+            if (".kts" !in args[0]) {
+                println("\n${ANSI_RED} The file should be an [kts] extension.\n")
+
+                exitProcess(7)
+            }
+
+            if (args.size > 1) {
+                val params = args[1]
+
+                this.execute(args[0], params)
+
+                return
+            }
+
+            this.execute(args[0])
+
+            return
+        }
+
         val directories = Files.list(Paths.get(".")).collect(Collectors.partitioningBy { Files.isDirectory(it) })
 
         val initFile = directories[false]?.filter { it.toString() == "./init.kts" }
 
         if (initFile?.isEmpty()!!)
-            println("\n${YELLOW_BACKGROUND_222}${ANSI_BLACK} There is no 'init.kts' file, create one using [create] command.\n")
+            println("\n${ANSI_YELLOW_229} There is no 'init.kts' file, create one using [create] command.\n")
         else {
             val finalInitPath = Paths.get("").toAbsolutePath().toString() + "/init.kts"
 
-            try {
-                val userPath = System.getProperty("user.home")
+            this.execute(finalInitPath)
+        }
+    }
 
-                val process = Runtime.getRuntime()
-                        .exec("$userPath/.koupper/helpers/octopusBootstrapper.sh $finalInitPath")
+    private fun execute(fileName: String, params: String = "EMPTY_PARAMS") {
+        var finalInitPath = ""
 
-                process.waitFor()
+        finalInitPath += if (isSingleFileName(fileName)) {
+            Paths.get("").toAbsolutePath().toString() + "/$fileName "
+        } else {
+            fileName
+        }.trim()
 
-                val errors = process.errorStream.bufferedReader().readText()
+        try {
+            val userPath = System.getProperty("user.home")
 
-                if (errors.isNotEmpty()) {
-                    print("$ANSI_RED$errors$ANSI_RESET")
+            val process = Runtime.getRuntime()
+                    .exec("$userPath/.koupper/helpers/octopusBootstrapper.sh $finalInitPath $params")
 
-                    exitProcess(7)
-                }
+            process.waitFor()
 
-                val output = process.inputStream.bufferedReader().readText()
+            val errors = process.errorStream.bufferedReader().readText()
 
-                print(output)
-            } catch (exception: IOException) {
-                println()
-                println(ANSI_RED + exception.printStackTrace())
-                println()
+            if (errors.isNotEmpty()) {
+                print("$ANSI_RED$errors$ANSI_RESET")
+
+                exitProcess(7)
             }
+
+            val output = process.inputStream.bufferedReader().readText()
+
+            print(output)
+        } catch (exception: IOException) {
+            println()
+            println(ANSI_RED + exception.printStackTrace())
+            println()
         }
     }
 
