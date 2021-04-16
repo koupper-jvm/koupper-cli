e4b25ef58e170c309ca321c7204cddb369c6c496
Jacob G. Acosta <jacob.gacosta@gmail.com>
2021-04-15 22:53:45 -0500

    Implement a caching system for update checking

--- src/main/kotlin/com/koupper/cli/CommandManager.kt
+++ src/main/kotlin/com/koupper/cli/CommandManager.kt
@@ -1,9 +1,12 @@
 package com.koupper.cli
 
+import com.koupper.cli.ANSIColors.ANSI_RED
 import com.koupper.cli.commands.*
 import com.koupper.cli.commands.AvailableCommands.HELP
 import com.koupper.cli.commands.AvailableCommands.NEW
 import com.koupper.cli.commands.AvailableCommands.RUN
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.IOException
 import java.net.URL
@@ -11,12 +14,38 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
+private val userPath = System.getProperty("user.home")
+
+private fun checkForUpdatesFrom(baseDate: String) {
+    try {
+        val process = Runtime.getRuntime()
+                .exec("$userPath/.koupper/helpers/octopusBootstrapper.sh UPDATING_CHECK")
+
+        process.waitFor()
+
+        val errors = process.errorStream.bufferedReader().readText()
+
+        if (errors.isNotEmpty()) {
+            print("$ANSI_RED$errors${ANSIColors.ANSI_RESET}")
+
+            exitProcess(7)
+        }
+
+        val version = File("$userPath/.koupper/helpers/.update_info")
+        version.createNewFile()
+
+        val output = process.inputStream.bufferedReader().readText()
+        val readyForUpdate = output == "AVAILABLE_UPDATES"
+
+        version.printWriter().use { out -> out.println("{\"last_request\": $baseDate, \"ready_for_update\": $readyForUpdate") }
+    } catch (exception: IOException) {
+        println("\n$ANSI_RED${exception.printStackTrace()}\n")
+    }
+}
+
+
 class CommandManager {
-    private val userPath = System.getProperty("user.home")
-
     fun process(arg: Array<String>) {
-        this.checkForUpdates()
-
         if (arg.isEmpty()) {
             DefaultCommand().execute()
 
@@ -42,71 +71,29 @@ class CommandManager {
         command.execute(*args)
     }
 
-    private fun checkForUpdates() {
-        val versionFile = File("$userPath/.koupper/helpers/.version")
+    fun askForUpdateFrom(baseDate: String) {
+        print("updates are available, Would you like apply them now? [y/n] ")
 
-        if (versionFile.exists()) {
-            val lastRequested = versionFile.readLines().first()
+        val updateInfo = File("$userPath/.koupper/helpers/.update_info")
 
-            val date = "\\D".toRegex().split(lastRequested).first {
-                it.isNotEmpty()
+        when (readLine()) {
+            "y", "Y" -> {
+                val content = URL("https://lib-installer.s3.amazonaws.com/updateme.txt").readText()
+
+                val file = File("$userPath/.koupper/helpers/updateme.kts")
+                file.writeText(content)
+                file.createNewFile()
+                file.setExecutable(true)
+                file.setReadable(true)
+                file.setWritable(true)
+                updateInfo.printWriter().use { out -> out.println("{\"last_request\": ${Date().time}, \"ready_for_update\": false}") }
             }
-
-            val timeElapsed = Date().time - Date(date.toLong()).time
-
-            val hoursElapsed = TimeUnit.MILLISECONDS.toHours(timeElapsed)
-
-            if (hoursElapsed.rem(2) == 0L) {
-                this.askForUpdating()
+            else -> {
+                updateInfo.printWriter().use { out -> out.println("{\"last_request\": $baseDate, \"ready_for_update\": true}") }
             }
         }
-    }
 
-    private fun askForUpdating() {
-        try {
-            val process = Runtime.getRuntime()
-                    .exec("$userPath/.koupper/helpers/octopusBootstrapper.sh UPDATING_CHECK")
-
-            process.waitFor()
-
-            val errors = process.errorStream.bufferedReader().readText()
-
-            if (errors.isNotEmpty()) {
-                print("${ANSIColors.ANSI_RED}$errors${ANSIColors.ANSI_RESET}")
-
-                exitProcess(7)
-            }
-
-            val output = process.inputStream.bufferedReader().readText()
-
-            if (output == "AVAILABLE_UPDATES") {
-                print("updates are available, Would you like apply them now? [y/n] ")
-
-                when (readLine()) {
-                    "y", "Y" -> {
-                        val content = URL("https://lib-installer.s3.amazonaws.com/updateme.txt").readText()
-
-                        val file = File("$userPath/.koupper/helpers/updateme.kts")
-                        file.writeText(content)
-                        file.createNewFile()
-                        file.setExecutable(true)
-                        file.setReadable(true)
-                        file.setWritable(true)
-
-                        exitProcess(0)
-                    }
-                    else -> {
-                        val version = File("$userPath/.koupper/helpers/.version")
-                        version.createNewFile()
-                        version.printWriter().use { out -> out.println("{\"last_request\": ${Date().time}}") }
-                    }
-                }
-            }
-        } catch (exception: IOException) {
-            println()
-            println(ANSIColors.ANSI_RED + exception.printStackTrace())
-            println()
-        }
+        exitProcess(0)
     }
 
     fun getCommandByName(input: String): Command {
@@ -127,6 +114,34 @@ class CommandManager {
     }
 }
 
-fun main(args: Array<String>) {
-    CommandManager().process(args)
+fun main(args: Array<String>) = runBlocking {
+    val commandManager = CommandManager()
+
+    val versionFile = File("$userPath/.koupper/helpers/.update_info")
+
+    if (versionFile.exists()) {
+        val updateInfo = versionFile.readLines().first()
+
+        val date = "\\D".toRegex().split(updateInfo).first {
+            it.isNotEmpty()
+        }
+
+        val timeElapsedSinceLastRequest = Date().time - Date(date.toLong()).time
+
+        val hoursElapsedSinceLastRequest = TimeUnit.MILLISECONDS.toHours(timeElapsedSinceLastRequest)
+
+        if (hoursElapsedSinceLastRequest >= 24) {
+            if ("true|false".toRegex().find(updateInfo)!!.value.toBoolean()) {
+                if (hoursElapsedSinceLastRequest.rem(4) == 0L) {
+                    commandManager.askForUpdateFrom(date)
+                }
+            } else {
+                launch {
+                    checkForUpdatesFrom(date)
+                }
+            }
+        }
+    }
+
+    commandManager.process(args)
 }
