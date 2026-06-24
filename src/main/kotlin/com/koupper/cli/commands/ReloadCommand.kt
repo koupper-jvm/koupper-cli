41f159bb329518b551033847705a2162a5dad736
Jacob G. Acosta <jacob.gacosta@gmail.com>
2026-06-24 12:30:38 -0600

    fix: reload command compilation error

--- src/main/kotlin/com/koupper/cli/commands/ReloadCommand.kt
+++ src/main/kotlin/com/koupper/cli/commands/ReloadCommand.kt
@@ -9,6 +9,8 @@ import java.io.PrintWriter
 import java.net.Socket
 
 class ReloadCommand : Command() {
+    override val name: String = "reload"
+
     override fun execute(vararg args: String): String {
         try {
             val host = System.getenv("KOUPPER_OCTOPUS_HOST") ?: "127.0.0.1"
